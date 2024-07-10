fcd272171f92bb7206c59de123f0916b03fca363
Aseru Kevin Ziyada <kayikayikevin7@gmail.com>
2024-07-10 14:52:22 +0300

    Add files via upload

---
+++ android/app/src/main/java/com/trancriber/lugandastt/FlutterMultiDexApplication.java
@@ -0,0 +1,6 @@
+package com.trancriber.lugandastt;
+
+import androidx.multidex.MultiDexApplication;
+
+public class FlutterMultiDexApplication extends MultiDexApplication {
+}
---
+++ android/app/src/main/kotlin/com/trancriber/lugandastt/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.trancriber.lugandastt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
